7720b73bc8056d5d5ad9b9fcfd2598ee09b74fb6
Ben Hanna <benpaulhanna@gmail.com>
2023-09-10 13:47:35 -0400

    Moved template manager to a global variable.

--- internal/routers/admin/router.go
+++ internal/routers/admin/router.go
@@ -8,42 +8,36 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type Admin struct {
-	TemplateManager *templates.TemplateManager
-}
+var tplMan *templates.TemplateManager = templates.New("./internal/templates/admin", ".mustache")
 
 func New() *chi.Mux {
 	r := chi.NewRouter()
-
-	t := templates.New("./internal/templates/admin", ".mustache")
-	a := &Admin{t}
-
-	r.Get("/", a.index)
-	r.Get("/products", a.products)
+	r.Get("/", getIndex)
+	r.Get("/products", getProducts)
 
 	return r
 }
 
-func (a *Admin) index(w http.ResponseWriter, r *http.Request) {
-	h, err := a.TemplateManager.RenderInLayout("index", "layout", map[string]string{"title": "Home"})
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	h, err := tplMan.RenderInLayout("index", "layout", map[string]string{"title": "Home"})
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 	} else {
-		HTML(w, h)
+		HTML(w, 200, h)
 	}
 }
 
-func (a *Admin) products(w http.ResponseWriter, r *http.Request) {
-	h, err := a.TemplateManager.RenderInLayout("products", "layout", map[string]string{"title": "Products"})
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	h, err := tplMan.RenderInLayout("products", "layout", map[string]string{"title": "Products"})
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 	} else {
-		HTML(w, h)
+		HTML(w, 200, h)
 	}
 }
 
-func HTML(w http.ResponseWriter, h string) {
+func HTML(w http.ResponseWriter, c int, h string) {
 	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(200)
+	w.WriteHeader(c)
 	io.WriteString(w, h)
 }
